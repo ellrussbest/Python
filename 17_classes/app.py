80d6307265fcf8c3b8e32a064d2bf4210fd3786b
Russell Best <ellrussbest@gmail.com>
2022-10-15 08:09:16 +0300

    minor changes

--- 17_classes/app.py
+++ 17_classes/app.py
@@ -1,37 +1,71 @@
-# you should take a look at magic methods
+# # you should take a look at magic methods
 
-class Point:
+# class Point:
 
-    #constructor
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+#     #constructor
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
 
-    def move(self):
-        print("move")
+#     def move(self):
+#         print("move")
     
 
-    def draw(self):
-        print("draw")
+#     def draw(self):
+#         print("draw")
 
-    def printPoint(self):
-        print(f"({self.x}, {self.y})")
-
-
-point1 = Point(0, 1)
-point1.draw()
-point1.move()
-point1.printPoint()
+#     def printPoint(self):
+#         print(f"({self.x}, {self.y})")
 
 
-class Person:
-    def __init__(self, name) -> None:
-        self.name = name
+# point1 = Point(0, 1)
+# point1.draw()
+# point1.move()
+# point1.printPoint()
+
+
+# class Person:
+#     def __init__(self, name) -> None:
+#         self.name = name
 
     
-    def talk(self):
-        print(f"Hi I am {self.name}")
+#     def talk(self):
+#         print(f"Hi I am {self.name}")
 
-russell = Person("Russell")
-russell.talk()
+# russell = Person("Russell")
+# russell.talk()
+
+class employee:
+    def __init__(self, firstname, lastname, salary):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.salary = salary
+        self.email = self.firstname + "." + self.lastname + "@russell.com"
+    
+    def giveRaise(self, salary):
+        self.salary = salary
+
+class developer(employee):
+    def __init__(self, firstname, lastname, salary, programming_language):
+        super().__init__(firstname, lastname, salary)
+        self.programming_languages = programming_language
+
+    def addLanguages(self, lang):
+        self.programming_languages.append(lang)
+
+employee1 = employee("job", "smith", 80000);
+print(employee1.salary)
+
+employee1.giveRaise(10000)
+
+print(employee1.salary)
+
+dev1 = developer("joe", "montana", 10000, ["python", "c"])
+print(dev1.salary)
+
+dev1.giveRaise(125000)
+
+print(dev1.salary)
+dev1.addLanguages("java")
+print(dev1.programming_languages)
---
+++ 24_Django/README.md
@@ -0,0 +1,7 @@
+pip install django==2.1
+
+django-admin startproject [name of project] [project folder path]
+
+python3 manage.py runserver
+
+python3 manage.py startapp [name of app]
